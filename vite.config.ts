1c7de85f1aea0afb6d8320cd20e701723780faba
Maxy IT <it@maxy.academy>
2026-07-08 03:04:11 +0000

    fix: set base path for intensive-class deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/intensive-class/ia-for-content-creation/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
